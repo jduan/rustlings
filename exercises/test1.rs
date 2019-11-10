2f50c912ca972275dbf02696eaa0d311747a0917
Jingjing Duan <duanjingjing@gmail.com>
2019-11-09 22:46:22 -0800

    finish primitive types

--- exercises/test1.rs
+++ exercises/test1.rs
@@ -8,7 +8,13 @@
 // the price of an order of apples given the order amount. No hints this time!
 
 // Put your function here!
-// fn ..... {
+fn calculate_apple_price(n: u32) -> u32 {
+    if n > 40 {
+        n
+    } else {
+        n * 2
+    }
+}
 
 // Don't modify this function!
 #[test]
